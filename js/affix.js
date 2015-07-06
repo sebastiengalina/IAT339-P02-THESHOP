f5c3d0ea46e7e6aab2a699793ff914c029ddf936
Sebastien Galina <sebastien@boldly.ca>
2015-07-05 17:06:11 -0700

    cleaned up a whole bunch of stuff

--- js/affix.js
+++ js/affix.js
@@ -2,11 +2,11 @@ $(document).ready(function() {
 	var affixItem = $("#affix-top");
 	console.log("affixItem "+affixItem);
 	$(window).scroll(function() {
+		var headerHeight = $("#header-main").height();
 		if ($(window).width() > 766) {
 			var affixItemTop = affixItem.offset().top;
 			if ($(window).scrollTop() > affixItemTop) {
 				affixItem.addClass("affixed");
-				console.log("affixed");
 			}
 		}
 	});
